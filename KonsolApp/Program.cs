ad08d31809669c2ec098eb240f00dad3cadac502
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-01-29 09:40:15 +0100

    4th attempt of school assignment.

--- KonsolApp/Program.cs
+++ KonsolApp/Program.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-
+// START Database builder
 var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
 {
     services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Code\ecutbildning\SQL\SQL3\SQL3\Infrastructure\Data\ProductCatalog.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True"));
-});
+}).Build(); 
+
+// STOP Database builder
